408e139eb3583affef1343737cf80455b9b8091e
Wes Yancy <79019393+wesyancy@users.noreply.github.com>
2025-07-07 00:20:01 -0500

    Minor changes to Krux page

--- src/pages/Krux/Krux.tsx
+++ src/pages/Krux/Krux.tsx
@@ -5,8 +5,7 @@ const Krux = () => {
     return (
         <div className='kruxPageDiv'>
             <h1>Krux</h1>
-            <p>This is the Krux Climbing App.</p>
-            <p>*App Currently in Beta*</p>
+            <p>This is the Krux Climbing App. *Currently in Alpha*</p>
             <p>Answer a few questions and Krux builds a training plan that fits your goals, experience, and available tools/equipment.</p>
             <p>As you log your workouts, Krux tracks your progress and dials up the challenge each week—so you are always leveling up.</p>
             <p>Krux isn’t a repackaged fitness app—it’s built specifically for climbers, with workouts that translate directly to better performance on the wall.</p>
